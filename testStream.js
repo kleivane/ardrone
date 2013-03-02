c96031a8ec73949fb3a90f500a52c71ea7420fb9
Tine Kleivane <kleivane@gmail.com>
2013-03-02 14:06:39 +0100

    Initial commit, forked from bjartwolf

--- testStream.js
+++ testStream.js
@@ -1,7 +1,6 @@
 var net = require('net');
-var rx = require('rx');
-//var ardrone = require('ar-drone');
-//var client = ardrone.createClient();
+var ardrone = require('ar-drone');
+var client = ardrone.createClient();
 require('./extendObservable.js'); // Adds the toObservable method to EventEmitter
 
 var RGBAStream = require('./RGBAStream');
@@ -12,22 +11,35 @@ var parser = new PaVEParser();
 var face = new FaceStream();
 var RGBA = new RGBAStream();
 var socket = net.connect({ host: '192.168.1.1', port: 5555});
-socket.pipe(parser).pipe(RGBA).pipe(face);
+socket.pipe(parser).pipe(RGBA)
 
-//client.takeoff();
+client.takeoff();
+client.after(5000, function(){
+    RGBA.on('data', function(data){
+        console.log("RGBA "+data.length);
+        var count = 0;
+        var side = 0;
+        for(var i = 0; i < data.length; i=i+4){
+            if(range(data[i], 190) && range(data[i+1], 55) && range(data[i+2],105)){
+                count++; 
+                side = side + (i%(240*4))/(4*240); 
+            }
+        };
+        side = side/count;
+        console.log("Verdi side: "+side);
+        if(side < 0.3){
+            client.counterClockwise(0.5);
+        }else if (side > 0.7){
+            client.clockwise(0.5);
+        }else{
+            client.stop();
+        }
+    })
+}).after(15000, function(){
+    client.stop();
+    client.land();
+});
 
-face.toObservable('data')
-    .select(function(faces) {
-        return faces[0];
-    })
-    .where(function (face) {
-        if (face) { return face.confidence > -1; }   
-    })
-    .select(function (face) {
-        return "Face at " + face.x + ", " + face.y + ". Width is " + face.width;
-    })
-    .subscribe(function (x) {
-        console.log(x);
-//        client.land();
-    })
-
+var range = function(rgba, match){
+    return (Math.abs(rgba - match) < 10);
+}
